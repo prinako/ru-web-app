6f147c5307862db1c38b248783c7812a98fa1673
prinako <nyankonton122@gmail.com>
2023-03-19 12:23:47 -0300

    on use helmet

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
 
-var app = express();
+const app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
