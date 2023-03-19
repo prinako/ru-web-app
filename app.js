05e4312b380eafac7fe80f69ed19b228afc9383e
prinako <nyankonton122@gmail.com>
2023-03-19 12:34:30 -0300

    remove helmwt

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const helmet = require('helmet');
 
 const app = express();
 
-app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
